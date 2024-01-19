86d03ec58c054e1e6b7db09495dc3623cdc2a5be
rahulll18 <109649389+rahulll18@users.noreply.github.com>
2024-01-20 01:34:01 +0530

    ui modified

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,4 +3,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Welcome } from "./Welcome";
 export { default as Footer } from "./Footer";
 export { default as Services } from "./Services";
-export { default as Transactions } from "./Transactions";
+// export { default as Transactions } from "./Transactions";
